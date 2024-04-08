63985fa4560b96d2ac76a3ead9bda5466d3a6650
Prashant Rajput <prshant.work@gmail.com>
2024-04-09 00:46:48 +0530

    error solved

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,11 @@ import { Suspense } from "react";
 
 const API_KEY = process.env.API_KEY
 
- export default async function Home({searchParams}) {
+ export default async function Home() {
   
-  const genre = searchParams.genre ||'fetchTrending';
+ 
 
- const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}`);
+ const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`);
  const data = await res.json();
  if(!res.ok){
   throw new Error('Failed to fetch')
